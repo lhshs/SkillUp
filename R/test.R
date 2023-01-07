dab4c5fd7c3b56d65c55ffa7bda315a6eb047812
hs.lion2@gmail.com <59005236+lhshs@users.noreply.github.com>
2023-01-07 19:40:27 +0900

    my_first_r

---
+++ R/test.R
@@ -0,0 +1,24 @@
+# 나는 할당을 해야 한다. ----
+a <- 1
+b <- 2
+
+plot(iris)
+
+R <- "hello world"
+R
+
+setwd('/Users/lhs/Desktop/GitHub')
+
+# 라이브러리 설치 ----
+# install.packages("dplyr")
+# install.packages("lubridate")
+# install.packages("stringr")
+# install.packages("data.table")
+
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(data.table)
+
+
+
